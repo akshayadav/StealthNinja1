ffbcc8c157df7fbc639717fe981b6b66c1262392
akshayadav <akshayadav@gmail.com>
2026-03-05 17:54:59 -0800

    Initial Commit

---
+++ NinjaRun1Tests/NinjaRun1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  NinjaRun1Tests.swift
+//  NinjaRun1Tests
+//
+//  Created by Akshay Yadav on 3/5/26.
+//
+
+import Testing
+@testable import NinjaRun1
+
+struct NinjaRun1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
